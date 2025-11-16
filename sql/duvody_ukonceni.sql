639d53d71a8c41c0938d56606e50d671143c26be
lenkakaniova <lenkakaniova@gmail.com>
2025-11-16 14:50:01 +0100

    uprava db, pridani souboru s hodnocenim cmr, dodelani duvodu ukonceni, uprava readme, vymazani cmr_zero_rows

--- sql/duvody_ukonceni.sql
+++ sql/duvody_ukonceni.sql
@@ -1,7 +1,5 @@
 
-
-
---takhle mi to pise 482514 radku
+--takhle mi to pise 482514 hovoru u neprijatych hovoru
 SELECT
     unique_call_id
 FROM fact_call
@@ -10,18 +8,7 @@ GROUP BY
 HAVING
     MIN(dateTimeConnect) IS NULL
 
---538890 radku ??
-SELECT
-    unique_call_id,
-    origCause_value
-FROM fact_call
-GROUP BY 
-    unique_call_id,
-    origCause_value
-HAVING
-    MIN(dateTimeConnect) IS NULL
-
---duvody ukonceni u vsech jedinecnych hovoru z posledniho radku (2234015) na strane volaneho
+--duvody ukonceni u vsech jedinecnych hovoru z posledniho radku (2234015) na strane volajiciho
 ;WITH last_rows AS (
     SELECT
         unique_call_id,
@@ -39,8 +26,47 @@ SELECT
 FROM last_rows
 WHERE rn = 1
 
+--duvody ukonceni hovoru z posledniho radku na strane volajiciho
+;WITH last_rows AS (
+    SELECT
+        unique_call_id,
+        origCause_value,
+        dateTimeConnect,
+        ROW_NUMBER() OVER (
+            PARTITION BY unique_call_id
+            ORDER BY dateTimeDisconnect DESC
+        ) AS rn
+    FROM fact_call
+)
+SELECT
+    origCause_value,
+    COUNT(*) AS pocet
+FROM last_rows
+WHERE rn = 1
+GROUP BY origCause_value
+ORDER BY pocet DESC
 
---duvody neprijeti hovoru na strane volaneho (482514)
+--duvody ukonceni hovoru z poslednuho radku na strane volaneho
+;WITH last_rows AS (
+    SELECT
+        unique_call_id,
+        destCause_value,
+        dateTimeConnect,
+        ROW_NUMBER() OVER (
+            PARTITION BY unique_call_id
+            ORDER BY dateTimeDisconnect DESC
+        ) AS rn
+    FROM fact_call
+)
+SELECT
+    destCause_value,
+    COUNT(*) AS pocet
+FROM last_rows
+WHERE rn = 1
+GROUP BY destCause_value
+ORDER BY pocet DESC
+
+--duvody neprijeti hovoru na strane volajiciho (482514)
 ;WITH calls AS 
     (
     SELECT
@@ -69,5 +95,37 @@ WHERE
     c.rn = 1
 GROUP BY 
     c.origCause_value
+ORDER BY
+    pocet_neprijatych DESC
+
+--duvody neprijeti hovoru na strane volaneho
+;WITH calls AS 
+    (
+    SELECT
+        unique_call_id,
+        destCause_value,
+        dateTimeConnect,
+        ROW_NUMBER() OVER (
+            PARTITION BY unique_call_id
+            ORDER BY dateTimeConnect DESC
+        ) AS rn
+    FROM fact_call
+    ),
+unanswered AS 
+    (
+    SELECT unique_call_id
+    FROM fact_call
+    GROUP BY unique_call_id
+    HAVING MIN(dateTimeConnect) IS NULL
+    )
+SELECT
+    c.destCause_value,
+    COUNT(*) AS pocet_neprijatych
+FROM calls c
+    JOIN unanswered u ON c.unique_call_id = u.unique_call_id
+WHERE 
+    c.rn = 1
+GROUP BY 
+    c.destCause_value
 ORDER BY
     pocet_neprijatych DESC
--- sql/readme.md
+++ sql/readme.md
@@ -2,6 +2,6 @@
 01. tvorba_db.sql                -vytvoreni dimenzi a faktove tabulky
 02. create_View_Duration.sql     - vytvoreni pohledu, ktery shrnuje informaci o trvani hovoru
 03. create_View_CMR_unique_calls.sql -vytvoreni pohledu nad CMR daty s unikátním identifikátorem hovoru  pro určení počtu unikátních hovorů
-04. cmr_zero_rows.sql            - pohled na nulove radky v soboru cmr
+04. kvalita_hovoru.sql            - hodnocení souboru cmr, vytvoření sloupce avg_mos s využitím regex
 05. duvody ukonceni.sql          - skripty k duvodum ukonceni pro cely soubor crd, duvody ukonceni jen zmeskanych hovoru
 06. statistika2                 -aktualizace s novými daty, distribuce hovorů dle měsíce, dnů a hodin, TOP10 volajících a volaných
